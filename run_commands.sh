c0467905ca3f2664a9c6cc80c71d57c9aecb4574
imonalc <taab7355tayz7355@gmail.com>
2023-12-18 22:35:42 +0900

    add 5PA results and new sim

--- run_commands.sh
+++ run_commands.sh
@@ -14,13 +14,13 @@ python3 extract_keypoints.py --datas Room --descriptors orb --solver GSM_wRT --i
 
 
 # indoor
-python3 extract_keypoints.py --datas Room Classroom Realistic Interior1 Interior2 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "5PA" --points 500
-python3 extract_keypoints.py --datas Room Classroom Realistic Interior1 Interior2 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "5PA" --points 1000
-python3 extract_keypoints.py --datas Room Classroom Realistic Interior1 Interior2 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "5PA" --points 10000
+python3 extract_keypoints.py --datas Room Classroom Realistic Interior1 Interior2 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "8PA" --points 500
+python3 extract_keypoints.py --datas Room Classroom Realistic Interior1 Interior2 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "8PA" --points 1000
+python3 extract_keypoints.py --datas Room Classroom Realistic Interior1 Interior2 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "8PA" --points 10000
 
 # outdoor
-python3 extract_keypoints.py --datas Urban1 Urban2 Urban3 Urban4 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "5PA" --points 500
-python3 extract_keypoints.py --datas Urban1 Urban2 Urban3 Urban4 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "5PA" --points 1000
-python3 extract_keypoints.py --datas Urban1 Urban2 Urban3 Urban4 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "5PA" --points 10000
+python3 extract_keypoints.py --datas Urban1 Urban2 Urban3 Urban4 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "8PA" --points 500
+python3 extract_keypoints.py --datas Urban1 Urban2 Urban3 Urban4 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "8PA" --points 1000
+python3 extract_keypoints.py --datas Urban1 Urban2 Urban3 Urban4 --descriptors orb torb sift tsift spoint tspoint sphorb alike talike Ltspoint Ftspoint Proposed Proposed01 Proposed1 Proposed10 Proposed_un --solver GSM_wRT --inliers "8PA" --points 10000
 
 echo "完了"
