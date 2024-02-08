64de23985631efe53c515fedd8b275cfafa0618a
imonalc <taab7355tayz7355@gmail.com>
2024-02-08 11:04:23 +0900

    recover matching

---
+++ utils/matching.py
@@ -0,0 +1,277 @@
+import cv2
+
+import pandas as pd
+import numpy as np
+
+
+from utils.ransac   import *
+from utils.keypoint import *
+from utils.metrics  import *
+from utils.camera_recovering import *
+
+
+
+
+
+def sort_key(pts1, pts2, desc1, desc2, points):
+
+    ind1 = np.argsort(pts1[:,2].numpy(),axis = 0)[::-1]
+    ind2 = np.argsort(pts2[:,2].numpy(),axis = 0)[::-1]
+
+    max1 = np.min([points,ind1.shape[0]])
+    max2 = np.min([points,ind2.shape[0]])
+
+    ind1 = ind1[:max1]
+    ind2 = ind2[:max2]
+
+    pts1 = pts1[ind1.copy(),:]
+    pts2 = pts2[ind2.copy(),:]
+
+    desc1 = desc1[:,ind1.copy()]
+    desc2 = desc2[:,ind2.copy()]
+
+    pts1 = np.concatenate((pts1[:,:2], np.ones((pts1.shape[0],1))), axis = 1 )
+    pts2 = np.concatenate((pts2[:,:2], np.ones((pts2.shape[0],1))), axis = 1 )
+
+    desc1 = np.transpose(desc1,[1,0]).numpy()
+    desc2 = np.transpose(desc2,[1,0]).numpy()
+
+    return pts1, pts2, desc1, desc2
+
+
+
+def mnn_matcher_old(desc1, desc2, use_new_method):
+    sim = desc1 @ desc2.transpose()
+    sim = (sim - np.min(sim))/ (np.max(sim) - np.min(sim))
+    if use_new_method == 1:
+        dec = 1
+    elif use_new_method == 2:
+        dec = 0.1
+    elif use_new_method == 3:
+        dec = 0.5
+    elif use_new_method == 4:
+        dec = 3
+    elif use_new_method == 5:
+        dec = 5
+    elif use_new_method == 6:
+        dec = 10
+    elif use_new_method == 7:
+        dec = 100
+    threshold = np.percentile(sim, 100-dec)
+    
+    sim[sim < threshold] = 0
+    nn12 = np.argmax(sim, axis=1)
+    nn21 = np.argmax(sim, axis=0)
+    ids1 = np.arange(0, sim.shape[0])
+    mask = (ids1 == nn21[nn12])
+    matches = np.stack([ids1[mask], nn12[mask]])
+    return matches.transpose()
+
+
+def mnn_matcher(desc1, desc2, use_new_method):
+    d1_square = np.sum(np.square(desc1), axis=1, keepdims=True)
+    d2_square = np.sum(np.square(desc2), axis=1, keepdims=True)
+    distances = np.sqrt(d1_square - 2 * np.dot(desc1, desc2.T) + d2_square.T)
+
+    distances = (distances - np.min(distances)) / (np.max(distances) - np.min(distances))
+
+    # しきい値の設定
+    dec = 5
+    threshold = np.percentile(distances, dec)  # 距離が小さいほど良いため、下位パーセンタイルを使用
+
+    # しきい値以上の距離を持つ要素を除外
+    distances[distances > threshold] = np.max(distances) + 1  # 最大距離より大きな値で除外
+
+    # 相互最近傍マッチング
+    nn12 = np.argmin(distances, axis=1)
+    nn21 = np.argmin(distances, axis=0)
+    ids1 = np.arange(0, distances.shape[0])
+    mask = (ids1 == nn21[nn12])
+    matches = np.stack([ids1[mask], nn12[mask]])
+    return matches.transpose()
+
+
+def matched_points(pts1, pts2, desc1, desc2, opt, args_opt, match='ratio', use_new_method=0):
+    if opt[-1] == 'p':
+        porce = int(opt[:-1])
+        n_key = int(porce/100 * pts1.shape[0])
+    else:
+        n_key = int(opt)
+
+    s_pts1  = pts1.copy()[:n_key,:]
+    s_pts2  = pts2.copy()[:n_key,:]
+    s_desc1 = desc1.copy().astype('float32')[:n_key,:]
+    s_desc2 = desc2.copy().astype('float32')[:n_key,:]
+
+    if 'orb' in args_opt:
+        s_desc1 = s_desc1.astype(np.uint8)
+        s_desc2 = s_desc2.astype(np.uint8)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, True)
+        matches = bf.match(s_desc1, s_desc2)
+    elif use_new_method in [1, 2, 3, 4, 5, 6, 7]:
+        matches_idx = mnn_matcher(s_desc1, s_desc2, use_new_method)
+        matches = [cv2.DMatch(i, j, 0) for i, j in matches_idx]
+    elif use_new_method == 10:
+        thresh = 0.75
+        bf = cv2.BFMatcher(cv2.NORM_L2, False)
+        matches1 = bf.knnMatch(s_desc1,s_desc2, k=2)
+        matches2 = bf.knnMatch(s_desc2,s_desc1, k=2)
+        good1 = []
+        for m, n in matches1:
+            if m.distance < thresh * n.distance:
+                good1.append(m)
+        good2 = []
+        for m, n in matches2:
+            if m.distance < thresh * n.distance:
+                good2.append(m)
+        good = []
+        for m1 in good1:
+            for m2 in good2:
+                if m1.queryIdx == m2.trainIdx and m1.trainIdx == m2.queryIdx:
+                    good.append(m1)
+                    break
+        matches = good
+    elif use_new_method == 11:
+        thresh = 0.75
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(s_desc1, s_desc2, k=2)
+        good = []
+        for m,n in matches:
+            if m.distance < thresh * n.distance:
+                good.append(m)
+        matches = good
+    elif match == 'ratio':
+        thresh = 0.75
+        bf = cv2.BFMatcher(cv2.NORM_L2, False)
+        matches = bf.knnMatch(s_desc1,s_desc2, k=2)
+        good = []
+        for m,n in matches:
+            if m.distance < thresh * n.distance:
+                good.append(m)
+        matches = good
+    else:
+        raise ValueError("Invalid matching method specified.")
+    
+
+
+    M = np.zeros((2,len(matches)))
+    for ind, match in zip(np.arange(len(matches)),matches):
+        M[0,ind] = match.queryIdx
+        M[1,ind] = match.trainIdx
+
+
+    return s_pts1, s_pts2, s_pts1[M[0,:].astype(int),:3], s_pts2[M[1,:].astype(int),:3]
+
+
+
+def get_error(x1, x2, Rx, Tx):
+
+    S = computeEssentialMatrixByRANSAC(x1, x2)
+    I = S[1]
+    I = I.astype(np.int64)
+
+    x1 = x1[I,:]
+    x2 = x2[I,:]
+
+    F = calc_ematrix(x1,x2)
+
+
+    R1,R2,T1,T2 = decomposeE(F)
+
+    R,T = choose_rt(R1,R2,T1,T2,x1,x2)
+
+    R_error = r_error(Rx,R)
+    T_error = t_error(Tx,T)
+
+    return R_error, T_error
+
+
+
+def get_descriptor(descriptor):
+    if descriptor == 'sphorb':
+        return 'sphorb', 'erp', 640, 0
+    elif descriptor == 'sift':
+        return 'sift', 'erp', 512, 0
+    elif descriptor == 'tsift':
+        return 'sift', 'tangent', 512, 0
+    elif descriptor == 'orb':
+        return 'orb', 'erp', 512, 0
+    elif descriptor == 'torb':
+        return 'orb', 'tangent', 512, 0
+    elif descriptor == 'spoint':
+        return 'superpoint', 'erp', 512, 0
+    elif descriptor == 'tspoint':
+        return 'superpoint', 'tangent', 512, 0
+    elif descriptor == 'alike':
+        return 'alike', 'erp', 512, 0
+    elif descriptor == 'talike':
+        return 'alike', 'tangent', 512, 0
+    elif descriptor == 'Proposed1':
+        return 'superpoint', 'tangent', 512, 1
+    elif descriptor == 'Proposed01':
+        return 'superpoint', 'tangent', 512, 2
+    elif descriptor == 'Proposed05':
+        return 'superpoint', 'tangent', 512, 3
+    elif descriptor == 'Proposed3':
+        return 'superpoint', 'tangent', 512, 4
+    elif descriptor == 'Proposed5':
+        return 'superpoint', 'tangent', 512, 5
+    elif descriptor == 'Proposed10':
+        return 'superpoint', 'tangent', 512, 6 
+    elif descriptor == 'Proposed_nolimit':
+        return 'superpoint', 'tangent', 512, 7
+    elif descriptor == 'Ltspoint':
+        return 'superpoint', 'tangent', 512, 10
+    elif descriptor == 'Ftspoint':
+        return 'superpoint', 'tangent', 512, 11
+
+
+
+def AUC(ROT, TRA, MET, L):
+
+    RAUC  = np.zeros(len(L))
+    TAUC  = np.zeros(len(L))
+
+    for index, t in enumerate(L):
+        ids = np.where(ROT<np.radians(t))[0]
+        RAUC[index] = len(ids)/len(ROT)
+
+    for index, t in enumerate(L):
+        ids = np.where(TRA<np.radians(t))[0]
+        TAUC[index] = len(ids)/len(TRA)
+
+    return RAUC, TAUC, np.array(MET)
+
+
+
+def get_data(DATAS):
+    if len(DATAS) == 1:
+        data = DATAS[0]
+    elif set(['Urban1','Urban2','Urban3','Urban4']) == set(DATAS):
+        data = 'Outdoor'
+    elif set(['Realistic','Interior1','Interior2','Room','Classroom']) == set(DATAS):
+        data = 'Indoor'
+    elif set(['Urban1_R','Urban2_R','Urban3_R','Urban4_R','Realistic_R','Interior1_R','Interior2_R','Room_R','Classroom_R']) == set(DATAS):
+        data = 'OnlyRot'
+    elif set(['Urban1_T','Urban2_T','Urban3_T','Urban4_T','Realistic_T','Interior1_T','Interior2_T','Room_T','Classroom_T']) == set(DATAS):
+        data = 'OnlyTra'
+    else:
+        data = ''
+        for DA in DATAS:
+            data+=DA
+
+    return data
+
+
+
+def get_kd(array):
+
+    array = np.array(array)
+    delimiter = int(array[-1])
+    A = array[:-1]
+    K = A[:delimiter].reshape(-1,3)
+    D = A[delimiter:].reshape(-1,32)
+    return K,D
